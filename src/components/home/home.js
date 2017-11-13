a4e5577cdd4916ce6321e520716d59168ecc0b48
Damith <damithasanka@gmail.com>
2017-11-14 00:59:48 +0530

    add learn more link

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
 let Home = ()=>{
     return (
         <div className="jumbotron">
         <h1>Pluralsight Adminstration</h1>
+        <Link to="/about/some" className="btn btn-primary btn-lg">Learn more</Link>
       </div>
     );
 }
